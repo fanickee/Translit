4fc33a3b7231dd6ad0041ec2d044f0d2e1ad029e
fanickee <fanickee@example.com>
2025-10-20 17:39:23 +0800

    single instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -110,6 +110,12 @@ fn main() {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _, _| {
+            let app = app.get_webview_window("main")
+                .expect("no main window");
+            app.show().unwrap();
+            app.set_focus().unwrap();
+        }))
         .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
